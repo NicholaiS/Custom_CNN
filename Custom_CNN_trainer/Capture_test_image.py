113b9271cdea92357a1d627f2dda14dd2f0d99e2
NicholaiS <nicholai.h.skov@hotmail.com>
2023-11-28 23:46:36 +0100

    Small changes and added test file

--- Custom_CNN_trainer/Capture_test_image.py
+++ Custom_CNN_trainer/Capture_test_image.py
@@ -1,12 +1,7 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
 
-# Load your pre-trained model
-model = tf.keras.models.load_model('custom_model_weights.h5')
-
-def preprocess_and_display_image(image_path):
+def capture_and_save_image(image_path):
     # Capture an image using cv2
     cap = cv2.VideoCapture(0)  # Change to 1 if using an external camera
     ret, frame = cap.read()
@@ -21,13 +16,6 @@ def preprocess_and_display_image(image_path):
     # Normalize pixel values to range between 0 and 1
     normalized_image = resized_image / 255.0
     
-    # Display the image using Matplotlib in a larger size
-    plt.figure(figsize=(8, 8))  # Adjust the figure size as needed
-    plt.imshow(normalized_image, cmap='gray')  # Change 'gray' to other colormaps if needed
-    plt.axis('off')  # Hide axes
-    plt.title('Resized Image')
-    plt.show()
-
     # Flatten the normalized resized image
     flattened_image = normalized_image.flatten()
     
@@ -35,18 +23,8 @@ def preprocess_and_display_image(image_path):
     np.savetxt(image_path, flattened_image.reshape(1, -1), delimiter=',', fmt='%f')
     print(f"Image saved as {image_path}")
 
-    # Prepare the image for prediction
-    image = normalized_image.reshape(1, 30, 40, 1)  # Assuming the input shape of your model is (30, 40, 1)
-
-    # Get the prediction for the image
-    prediction = model.predict(image)
-    predicted_class = np.argmax(prediction)
-
-    # Print the predicted class
-    print(f"Predicted class: {predicted_class}")
-
 # Path to save the image as a text file
 image_save_path = "resized_image.txt"
 
-# Call the function to capture, preprocess, display the image, and run inference before saving it as a text file
-preprocess_and_display_image(image_save_path)
+# Call the function to capture the image and save it as a text file
+capture_and_save_image(image_save_path)
